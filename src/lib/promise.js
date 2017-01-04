2da1a05a3ba2d71453c21c3d88f3239626d20a42
fiveSecondsRealMan <fiveSecondsRealMan>
2017-01-04 15:09:01 +0800

    promise

--- src/lib/promise.js
+++ src/lib/promise.js
@@ -22,21 +22,37 @@
     基础扩展（日期处理、工具库）
     ui组件（按钮、面板、对话框、Tab、lightbox）
     实时动画系统(平滑动画)
+
+    1、异步流程控制库，类似书写同步代码一样书写异步代码
+    2、promise只允许运行期间只存在一个状态，传递方向是单向且不可逆的
+    3、提供核心then方法，
+      1)、该方法会产生一个新的promise对象，方便链式调用
+      2)、利用闭包特性传入当前promise和新创建的promise以及状态函数
+        异常检测旧状态函数
+        得到旧状态函数的值，如果是新promise对象则抛出异常
+        如果捕获到异常，那么直接调用新创建的promise的错误状态函数
+        将结果传入到新promise对象的成功状态函数
+
+      3)、并且为当前调用then方法的promise对象绑定状态回调函数
+    4、promise为异步调用状态函数，等promise链完全生成
+    5、状态函数发生异常时，只能传给下一个promise的reject函数，不能传给自身的reject
+
 **/
 
 define(() => {
 
   // promise状态
   const __STATE__ = {
-    pending: 0,
-    resolved: 1,
-    rejected: 2
+    PENDING: 0,
+    RESOLVE: 1,
+    REJECT: 2
   };
 
-  // 函数延迟执行
+  // 函数异步执行
   const nextTick = (() => {
     const setImmediate = window.setImmediate;
-    const setTimeout = window.setTimeout, res;
+    const setTimeout = window.setTimeout;
+    let res;
 
     if (setImmediate && typeof setImmediate === 'function')
       res = setImmediate;
@@ -52,18 +68,33 @@ define(() => {
     if (curr.state === state)
       nextTick(() => callback(curr.data));
 
-    else if (state === __STATE__.resolved)
+    else if (state === __STATE__.REJECT)
       curr.resolveCallbacks.push(callback);
 
-    else if (state === __STATE__.rejected)
+    else if (state === __STATE__.REJECT)
       curr.rejectCallbacks.push(callback);
   }
 
-  function fire () {
+  function fire (curr) {
+    let callbacks, info;
 
+    if (curr.state === __STATE__.RESOLVE) {
+      callbacks = curr.resolveCallbacks;
+      info = curr.data;
+    } else {
+      callbacks = curr.rejectCallbacks;
+      info = curr.reason;
+    }
+
+    if (!callbacks.length)
+      return;
+
+    nextTick(() => {
+      callbacks.forEach(callback => callback(info));
+    });
   }
 
-  function processCallback (curr, next, callback) {
+  function processCallback (next, callback) {
     return info => {
       try {
         let value = callback(info);
@@ -93,7 +124,7 @@ define(() => {
   }
 
   function Promise () {
-    this.state = __STATE__.pending;
+    this.state = __STATE__.PENDING;
     this.data = [];
     this.reason = '';
     this.resolveCallbacks = [];
@@ -103,31 +134,75 @@ define(() => {
   // 定义Promise静态方法
   Object.assign(Promise, {
     createPromise(fn) {
-      const promiseIns = new Promise();
-
+      const promiseIns = new this();
       fn && fn(promiseIns);
 
       return promiseIns;
     },
 
-    parsePromise() {
-
+    resolved(data) {
+      return this.createPromise(
+        promiseIns => promiseIns.resolve(data)
+      );
     },
 
-    resolve() {
-
+    rejected(reason) {
+      return this.createPromise(
+        promiseIns => promiseIns.reject(reason)
+      );
     },
 
-    reject() {
+    /**
+     * @param {Array|<Promise>} promiseIns promise数组
+    */
+    all(promiseInsArr) {
+      !Array.isArray(promiseInsArr) && (promiseInsArr = [promiseInsArr]);
+      const currPromise = this.createPromise();
+      const res = [];
+      let promiseCount = 0;
 
+      function createResolveHandler (index) {
+        return data => {
+          res[index] = data;
+          promiseCount++;
+
+          if (index >= promiseInsArr.length)
+            currPromise.resolve.apply(currPromise, res);
+        };
+      }
+
+      promiseInsArr.forEach((ins, index) =>
+        ins.then(
+          createResolveHandler(index),
+          reason => currPromise.reject(reason)
+        )
+      );
+
+      return currPromise;
     },
 
-    all() {
+    race(promiseInsArr) {
+      !Array.isArray(promiseInsArr) && (promiseInsArr = [promiseInsArr]);
+      const currPromise = this.createPromise();
+      let promiseCount = 0;
 
-    },
+      function createResolveHandler (index) {
+        return data => {
+          promiseCount++;
 
-    race() {
+          if (promiseCount >= 1)
+            currPromise.resolve(data);
+        }
+      }
 
+      promiseInsArr.forEach((ins, index) =>
+        ins.then(
+          createResolveHandler(index),
+          reason => currPromise.reject(reason)
+        )
+      );
+
+      return currPromise;
     }
   });
 
@@ -142,29 +217,49 @@ define(() => {
 
       // 如果是成功状态回调函数
       if (typeof onResolve === 'function') {
-        onResolve = processCallback(currPromiseIns, nextPromiseIns, onResolve);
+        onResolve = processCallback(nextPromiseIns, onResolve);
       } else {
-
+        onResolve = data => nextPromiseIns.resolve(data);
       }
 
       // 绑定成功状态回调函数
-      addListener(currPromiseIns, __STATE__.resolved, onResolve);
+      addListener(currPromiseIns, __STATE__.RESOLVE, onResolve);
 
       // 如果是失败状态回调函数
       if (typeof onReject === 'function') {
         onReject = processCallback(currPromiseIns, nextPromiseIns, onReject);
       } else {
-
+        onReject = reason => nextPromiseIns.reject(data);
       }
 
       // 绑定失败状态回调函数
-      addListener(currPromiseIns, __STATE__.rejected, onReject);
+      addListener(currPromiseIns, __STATE__.REJECT, onReject);
 
       return nextPromiseIns;
     },
 
     catch(onReject) {
       return this.then(null, onReject);
+    },
+
+    resolve(data) {
+      if (this.state !== __STATE__.PENDING)
+        return;
+
+      this.state = __STATE__.RESOLVE;
+      this.data = data;
+
+      fire(this);
+    },
+
+    reject(reason) {
+      if (this.state !== __STATE__.PENDING)
+        return;
+
+      this.state = __STATE__.REJECT;
+      this.reason = reason;
+
+      fire(this);
     }
   });
 
